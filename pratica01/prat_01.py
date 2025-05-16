b83045c26ee1cbbd39de49c268fd7e644bc81181
SNina11 <sandra.nina@icloud.com>
2025-05-16 20:14:56 -0300

    new file:   pratica01/prat_01.py

---
+++ pratica01/prat_01.py
@@ -0,0 +1,52 @@
+#Programa de Saudacao
+#Crie um programa que imprima a mensagem "Olá, mundo!" na tela.
+print ("Olá, mundo!")
+
+#Calculadora de soma
+"""- Calculadora de Soma
+* Desenvolva um programa que soma dois números. 
+Use as variáveis numero1 = 12 e numero2 = 14. O programa deve calcular a soma e exibir o resultado.
+"""
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+print(f"soma: {soma}")
+
+"""
+3- Calculadora de Volume
+* Crie um programa que calcula o volume de uma caixa retangular. Use as seguintes dimensões:
+
+* Comprimento: 12 cm
+* Largura: 14 cm
+* Altura: 20 cm
+O programa deve calcular o volume e exibir o resultado em cm³.
+"""
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento * largura * altura
+print(f"Volume: {volume} cm³")
+
+
+"""
+4- Calculadora de Preço Total
+* Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
+
+* Nome do produto: "Cadeira Infantil"
+* Preço unitário: R$ 12.40
+* Quantidade: 3
+O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final.
+"""
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+preco_total = quantidade * preco_unitario
+print(f"Produto: {nome_produto}")
+print(f"Preço unitário: R$ {preco_unitario:.2f}")
+print(f"Quantidade: {quantidade}")
+print(f"Preço total: R$ {preco_total:.2f}")
+
+#git config --global user.name "SNina11"
+#git config --global user.email "sandra.nina@icloud.com"
+
+#
